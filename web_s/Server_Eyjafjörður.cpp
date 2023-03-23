55bf7813ba0c45a9e4026cb52e30eb92d6472960
Mohamed khalil Naqqad <mnaqqad@e1r4p11.1337.ma>
2023-03-23 14:02:29 +0000

    tempp_push

--- web_s/Server_Eyjafjörður.cpp
+++ web_s/Server_Eyjafjörður.cpp
@@ -1,28 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Server_Eyjafjörður.hpp                             :+:      :+:    :+:   */
+/*   Server_Eyjafjörður.cpp                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mnaqqad <mnaqqad@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/21 16:54:59 by mnaqqad           #+#    #+#             */
-/*   Updated: 2023/03/23 13:51:10 by mnaqqad          ###   ########.fr       */
+/*   Updated: 2023/03/23 13:50:39 by mnaqqad          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#pragma once
-#include <deque>
-#include "header.hpp"
+#include "Server_Eyjafjörður.hpp"
 
-class Server_Eyjafjörður{
-    public:
-        struct addrinfo hints , *servinfo;
-        Server_Eyjafjörður();
-        Server_Eyjafjörður(int sk);
-        int Set_up_Server();
-    private:
-        int Server_Socket;
-        // std::deuque<>
-        // int Status_Code;
-        
-};
+Server_Eyjafjörður::Server_Eyjafjörður(){}
+
+
+Server_Eyjafjörður::Server_Eyjafjörður(int sk):Server_Socket(sk){
+    
+}
+
+int Server_Eyjafjörður::Set_up_Server(){
+    
+}
--- web_s/web_main.cpp
+++ web_s/web_main.cpp
@@ -6,12 +6,13 @@
 /*   By: mnaqqad <mnaqqad@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/10 17:03:19 by mnaqqad           #+#    #+#             */
-/*   Updated: 2023/03/21 16:55:36 by mnaqqad          ###   ########.fr       */
+/*   Updated: 2023/03/23 12:47:04 by mnaqqad          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "header.hpp"
 // #include "Client.hpp"
+#include "Server_Eyjafjörður.hpp"
+#include "Client_Smár.hpp"
 
 #define PORT 80
 #define backlog 20
@@ -70,225 +71,36 @@ int main(){
                         "<html>im the client here is you response</html>\r\n");
 
     //////----connection and binding ------------////
+    struct addrinfo hints , *servinfo;
+
+    memset(&hints, 0, sizeof(hints));
     
-   int server_socket = socket(AF_INET,SOCK_STREAM,0);
-   if (setsockopt(server_socket,SOL_SOCKET,SO_REUSEADDR,&reuse,sizeof(reuse)) < 0){
-        perror("setsockopt failed");
-        return (0);
-   }
-    if (server_socket == -1){
-            perror("Error in creating a socket\n");
-            return (0);
+    // hints.ai_family = AF_UNSPEC;
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_PASSIVE;
+
+    const char *port = "8080";
+    int get_add_errno = 0;
+    if ((get_add_errno = getaddrinfo(NULL, port, &hints, &servinfo)) != 0){
+        std::cerr << (get_add_errno) << std::endl;
     }
-    std::cout<<"Socket created !!\n";
-    struct sockaddr_in host_addd;
-    int host_addlen = sizeof(host_addd);
-
-    host_addd.sin_family = AF_INET;
-    host_addd.sin_port = htons(PORT);
-    host_addd.sin_addr.s_addr = htonl(INADDR_ANY);
-
-    struct sockaddr_in client_addd;
-    int client_addlen = sizeof(client_addd);
-    assert(bind(server_socket, (struct sockaddr *)&host_addd, host_addlen) == 0);
-    // if (bind(server_socket, (struct sockaddr *)&host_addd, host_addlen) != 0){
-    //     perror("Bind error");
-    //     return (0);
-    // }
-    std::cout << "Socket bound to address\n";
-
-    if (listen(server_socket, backlog) != 0){
-        perror("Im deaf");
-        return (0);
+    char hold_ip[INET_ADDRSTRLEN];
+    // inet_ntop(server_struct->sin_family,&(server_struct->sin_addr),hold_ip,sizeof(hold_ip));
+    int server_socket = socket(servinfo->ai_family,servinfo->ai_socktype,servinfo->ai_protocol);
+    if (setsockopt(server_socket,SOL_SOCKET,SO_REUSEADDR,&reuse,sizeof(reuse)) < 0){
+        perror("Setsockopt Error");
+        exit(EXIT_FAILURE);
     }
-
-    std::cout << "Server listening\n";
-    
-    //r- ---------SELECT--------------
-    
-    // fd_set read_fds;
-    // fd_set write_fds;
-
-    // FD_ZERO(&read_fds);
-    // FD_ZERO(&write_fds);
-    // FD_SET(server_socket,&read_fds);
-    // FD_SET(server_socket,&write_fds);
-    // int f_max = server_socket;
-    // struct timeval time;
-    // time.tv_sec = 5;
-    // time.tv_usec = 0;
-    
-    // for(;;){
-    //     fd_set copy_read = read_fds;
-    //     fd_set copy_write = write_fds;
-    //    int rdc = select(f_max + 1, &copy_read,&copy_write,nullptr,nullptr);
-    //    if (rdc < 0){
-    //         perror("select error");
-    //         break;
-    //    }
-    //    std::cout << rdc << "for the wi\n";
-    //    if (rdc == 0){
-    //      perror("select timeout");
-    //      break;
-    //    }
-
-    //     for(int i = 0; i <= f_max; i++){
-    //             int sock = i;
-    //         if (FD_ISSET(sock,&copy_read)){
-    //          if (sock == server_socket){
-    //             // ACCPET CONNECTION
-    //             int client_socket = accept(server_socket,reinterpret_cast<struct sockaddr*>(&host_addd),
-    //                         reinterpret_cast<socklen_t*>(&host_addlen));
-    
-    //             if (client_socket < 0){
-    //                 std::cout << "Error in accepting socket\n";
-    //                 FD_CLR(client_socket,&read_fds);
-    //                 close(client_socket); 
-    //             }
-    //             if (recv(client_socket,buffer,BUFFER_SIZE,0) <= 0){
-    //                 close(client_socket);
-    //                 FD_CLR(client_socket,&read_fds);
-    //             }
-    //             std::cout << "Connetion made"<<std::endl;
-    //             FD_SET(client_socket,&read_fds);
-    //             if (client_socket >= f_max)
-    //                     f_max = client_socket;
-    //             std::string wel ("HTTP/1.1 200 OK\r\n\r\n"
-    //             "<header>Welcome to the server</header>\r\n");
-    //             send(client_socket,wel.c_str(),wel.size() + 1,0);
-    //         }
-    //         else{
-    //             if (recv(sock,buffer,BUFFER_SIZE,0) <= 0){
-    //                 close(sock);
-    //                 FD_CLR(sock,&read_fds);
-    //             }
-    //         }
-    //         }
-    //     }
-    // }
-    
-    //r-/////////////////////////////////////////////////////
-
-
-    //y- -----------------------------POLL------------------------------
-    
-    // int sockets_count = 0;
-    
-    // int poll_fd_size = 1;
-    
-    // struct pollfd *sockets_fds = static_cast<pollfd*>(malloc(sizeof(pollfd) * poll_fd_size));
-
-    // sockets_fds[0].fd = server_socket;
-    // sockets_fds[0].events = POLLIN;
-
-    // sockets_count++;
-    
-
-    // while(true){
-    //     int numbers_from_poll = poll(sockets_fds,sockets_count, -1);
-    //     if (numbers_from_poll == -1){
-    //         perror("poll error");
-    //         exit(1);
-    //     }
-    //     for(int i = 0; i < sockets_count; i++){
-    //         if (sockets_fds[i].revents & POLLIN){
-    //             if (sockets_fds[i].fd == server_socket){
-    //                  int client_socket = accept(server_socket,reinterpret_cast<struct sockaddr*>(&host_addd),
-    //                         reinterpret_cast<socklen_t*>(&host_addlen));
-    
-    //             if (client_socket < 0){
-    //                 std::cout << "Error in accepting socket\n";
-    //                 // erase_pfd(sockets_fds,i);
-    //                 close(client_socket); 
-    //             }
-    //             if (recv(client_socket,buffer,BUFFER_SIZE,0) <= 0){
-    //                 close(client_socket);
-    //                 erase_pfd(sockets_fds,client_socket);
-    //             }
-    //             std::cout << buffer << std::endl;
-    //             std::cout << "Connetion made"<<std::endl;
-    //             insert_pfd(sockets_fds,client_socket,sockets_count,poll_fd_size);
-    //             std::string wel ("HTTP/1.1 200 OK\r\n\r\n"
-    //             "<header>Welcome to the server</header>\r\n");
-    //             send(client_socket,wel.c_str(),wel.size() + 1,0);
-    //             }
-    //             else{
-    //                 if (recv(i,buffer,BUFFER_SIZE,0) <= 0){
-    //                 close(i);
-    //                 erase_pfd(sockets_fds,i);
-    //                 }
-    //             }
-    //         }
-    //     }
-    // }
-    
-    //y- ----------------------------------------------------------
-
-    //b- /////////////////////////////---KQUEUE---------/////////////////////
-    add_event(server_socket, EVFILT_READ | EVFILT_WRITE);
-    struct kevent evv[max_events];
-    for(;;){
-        int n_ev = kevent(kq,NULL,0,evv,max_events,NULL);
-        if (n_ev < 0){
-            perror("kevent");
-        }
-        for(int i = 0; i < n_ev; i++){
-            int fd = static_cast<int>(evv[i].ident);
-                if (evv[i].filter == EVFILT_READ){
-                    if (fd == server_socket){
-                // ACCPET CONNECTION
-                int client_socket = accept(server_socket,reinterpret_cast<struct sockaddr*>(&host_addd),
-                            reinterpret_cast<socklen_t*>(&host_addlen));
-    
-                if (client_socket < 0){
-                    std::cout << "Error in accepting socket\n";
-                    close(client_socket); 
-                }
-                add_event(client_socket , EVFILT_READ);
-                add_event(client_socket , EVFILT_WRITE);
-                std::cout << "Connetion made"<<std::endl;
-                std::string wel ("HTTP/1.1 200 OK\r\n\r\n"
-                "<header>Welcome to the server</header>\r\n");
-                send(client_socket,wel.c_str(),wel.size() + 1,0);
-                }
-                else {
-                     if (recv(fd,buffer,BUFFER_SIZE,0) <= 0){
-                        perror("recv");
-                        close(i);
-                    }
-                    std::cout << buffer << std::endl;
-                }
-            }
-        }
+    if (bind(server_socket,(struct sockaddr*)servinfo->ai_addr,servinfo->ai_addrlen) < 0){
+        perror("Bind Error");
+        exit(EXIT_FAILURE);
     }
-        
-    
-
-    // if (errno == EINTR){
-    //     std::cout << "signal\n";
-    //     return (0);
-    // }
-    
-
-    //b- ////////////////////////////////////////////////////////////
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
+    std::cout << "IP Bound" << std::endl;
+    if (listen(server_socket,SOMAXCONN) < 0){
+        perror("Listen Error");
+        exit(EXIT_FAILURE);
+    }
+    std::cout << "Server up and listening..." << std::endl;
+    recv(server_socket,buffer,BUFFER_SIZE,0);
 }
