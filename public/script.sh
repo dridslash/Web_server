b447561762d9271ed20012c89bd1a7745ec321a7
Mohamed khalil Naqqad <mnaqqad@e1r7p1.1337.ma>
2023-04-09 15:55:16 +0000

    empty

--- public/script.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-echo "Welcome to Shell Scripting"
--- web_main.cpp
+++
@@ -1,9 +0,0 @@
-#include "Server_Master.hpp"
-
-int main(int ac, char **argv){
-
-    Server_Master Main_server;
-
-    Main_server.Upp_ports(argv[1]);
-    Main_server.multiplexing();
-}
